dc1d0b8b92a7aa1e8d7597156694f1222db33762
zxpbenson <zxpbenson@gmail.com>
2021-12-14 14:14:01 +0800

    走api切换配置

--- src/main/java/com/benson/LOG_CFG.java
+++ src/main/java/com/benson/LOG_CFG.java
@@ -2,6 +2,7 @@ package com.benson;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,16 +10,16 @@ import java.io.File;
 
 public class LOG_CFG {
 
-    private static final Logger LOG = LoggerFactory.getLogger(LOG_CFG.class);
+    private static Logger LOG = LoggerFactory.getLogger(LOG_CFG.class);
 
     public static void main(String[] args) {
 
         //log4j2切换配置的方式
-        LoggerContext ctx = (LoggerContext)LogManager.getContext();
+        //LoggerContext ctx = (LoggerContext) LogManager.getContext(true);
         LOG.error("switching log4j config file : {}", "src/main/resources/log4j2.xml");
-        ctx.setConfigLocation(                                                               new File("src/main/resources/log4j2-v2.xml").toURI());
-        //ctx.setConfigLocation(new File("/Users/sefarious/workspace/github.com/zxpbenson/log4j-poc-demo/src/main/resources/log4j2-v2.xml").toURI());
-        //ctx.reconfigure();
+        //ctx.setConfigLocation(new File("src/main/resources/log4j2-v2.xml").toURI());
+        //ctx.reconfigure();//这个没用
+        Configurator.reconfigure(new File("src/main/resources/log4j2-v2.xml").toURI());//这个ok
         LOG.error("switching log4j config file done");
     }
 }
